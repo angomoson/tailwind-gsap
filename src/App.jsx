28749d0851700440c0b84b2906b7b18b1cd25379
angomoson <angomosn64@gmail.com>
2022-11-28 22:26:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <div className=" w-[100%] h-[100%] flex justify-center align-center">
+    <div className="w-[200px] h-[150px] bg-dimBlue text-primary flex justify-center align-center font-poppins ">
+      hello world
+    </div>
+  </div>
+);
+export default App;
